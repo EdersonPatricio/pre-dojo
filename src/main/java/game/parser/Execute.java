6f2fada212dac758b79a61aa9187221724980fad
edersonpatricio <edersonpatricio@edersonpatricio>
2015-11-04 15:09:10 -0300

    Retificado método main - incluída chamada da classe ExportFile para geração do arquivo final

--- src/main/java/game/parser/Execute.java
+++ src/main/java/game/parser/Execute.java
@@ -1,16 +1,31 @@
 package game.parser;
 
+import game.parser.entities.ExportFile;
+import game.parser.entities.Ranking;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.IOException;
 
 public class Execute {
 
-	public static void main( String[] args ) throws IOException {
-
+	public static void main( String[] args ) {
 		try {
-			BufferedReader readerFile = new BufferedReader( new FileReader( "game.log" ) );
+			// Ler o arquivo de log
+			BufferedReader readerFile = new BufferedReader( new FileReader( "Ranking.log" ) );
 
+			// Executa a leitura do arquivo para que possa percorrer cada linha
+			// e analisar os assassinatos e mortes de cada jogador
+			Ranking ranking = Parser.executeParseByFile( readerFile );
+
+			// Instanciando a classe ExportFile com a lista de games
+			ExportFile exportFile = new ExportFile( ranking );
+
+			// A partir da lista de games com sa devidas informações de cada
+			// jogador, será exportado um arquivo com as informações referentes
+			// à cada jogo
+			exportFile.exportFileWithResults();
+
+			System.out.println( "*******************|<-Finalizado!!->|*******************" );
 		} catch ( Exception e ) {
 			System.out.println( "*******************|<-Arquivo não localizado!!->|*******************" );
 		}
