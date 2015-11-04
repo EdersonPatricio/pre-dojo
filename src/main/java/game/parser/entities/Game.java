c9798ff26846c0deceb141c05c535a5b007a2cf5
edersonpatricio <edersonpatricio@edersonpatricio>
2015-11-04 11:32:08 -0300

    Criada classe que servirá para armazenar os dados referentes à cada jogo.

---
+++ src/main/java/game/parser/entities/Game.java
@@ -0,0 +1,49 @@
+package game.parser.entities;
+
+import java.util.Map;
+
+public class Game {
+
+	private String name;
+
+	private Integer totalKillsByGame;
+
+	private Map<String, Map<String, Player>> killsByPlayers;
+
+	public Game() {
+
+	}
+
+	public Game( String name ) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName( String name ) {
+		this.name = name;
+	}
+
+	public Integer getTotalKillsByGame() {
+		return totalKillsByGame;
+	}
+
+	public void setTotalKillsByGame( Integer totalKillsByGame ) {
+		this.totalKillsByGame = totalKillsByGame;
+	}
+
+	public Map<String, Map<String, Player>> getKillsByPlayers() {
+		return killsByPlayers;
+	}
+
+	public void setKillsByPlayers( Map<String, Map<String, Player>> killsByPlayers ) {
+		this.killsByPlayers = killsByPlayers;
+	}
+
+	@Override
+	public String toString() {
+		return "Game [name=" + name + ", totalKillsByGame=" + totalKillsByGame + ", killsByPlayers=" + killsByPlayers + "]";
+	}
+}
